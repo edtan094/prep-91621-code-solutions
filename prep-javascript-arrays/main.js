ecc9a760d12b490e30b44f759a0324610282ef55
Edwin Tan <etan094@gmail.com>
2021-09-28 13:20:59 -0700

    Complete prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,18 @@
+const languages = ['html', 'css', 'javascipt', 'react', 'node', 'postgres']
+;
+console.log(languages);
+
+var firstElement = 'html'
+;
+console.log('the first element of the array is ' + firstElement)
+;
+var thirdElement = 'javascipt'
+;
+console.log('the third element of the array is ' + thirdElement);
+
+var length = languages.length;
+console.log('The length of this array is ' + length)
+;
+var lastElement = 'postgres';
+console.log('The last element of the array is ' + lastElement)
+;
