540050dd0a1ee12648faa652cdbc914f8680d352
Edwin Tan <etan094@gmail.com>
2021-09-28 20:57:30 -0700

    made more adjustments to the assignment.

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -6,13 +6,17 @@ var firstElement = languages[0]
 ;
 console.log('the first element of the array is ' + firstElement)
 ;
-var thirdElement = 'javascipt'
+var thirdElement = languages[2]
 ;
 console.log('the third element of the array is ' + thirdElement);
 
 var length = languages.length;
 console.log('The length of this array is ' + length)
 ;
-var lastElement = 'postgres';
+
+var lastIndex = length - 1;
+
+var lastElement = languages[lastIndex];
+
 console.log('The last element of the array is ' + lastElement)
 ;
